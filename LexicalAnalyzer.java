f001a60fdec2e8fffea14b12ddc811019a49c86b
Puraneshi <41456546+Puraneshi@users.noreply.github.com>
2024-07-23 20:10:29 -0300

    update 21/07

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -1,13 +1,20 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LexicalAnalyzer {
     private String input;
     private int position;
+    private int lineNumber;
+    private Map<String, TokenType> symbolTable;
     
     public LexicalAnalyzer(String input) {
         this.input = input;
         this.position = 0;
+        this.lineNumber = 1;
+        this.symbolTable = new HashMap<>();
+        prepopulateSymbolTable();
     }
     
     public List<Token> analyze() {
@@ -16,7 +23,10 @@ public class LexicalAnalyzer {
         while (position < input.length()) {
             char currentChar = input.charAt(position);
             
-            if (Character.isWhitespace(currentChar)) {
+            if (currentChar == '\n') {
+                lineNumber++;
+                position++;
+            } else if (Character.isWhitespace(currentChar)) {
                 // Skip whitespace
                 position++;
             } else if (Character.isLetter(currentChar) || currentChar == '_') {
@@ -32,6 +42,7 @@ public class LexicalAnalyzer {
                     tokens.add(new Token(TokenType.KEYWORD, tokenValue));
                 } else {
                     tokens.add(new Token(TokenType.IDENTIFIER, tokenValue));
+                    symbolTable.putIfAbsent(tokenValue, TokenType.IDENTIFIER);
                 }
             } else if (Character.isDigit(currentChar)) {
                 // Integer or float constant
@@ -67,10 +78,25 @@ public class LexicalAnalyzer {
                         break;
                     case '+':
                     case '-':
+                    case '|':
+                        if (currentChar == '|' && position + 1 < input.length() && input.charAt(position + 1) == '|') {
+                            tokens.add(new Token(TokenType.ADDOP, "||"));
+                            position += 2;
+                        } else {
+                            tokens.add(new Token(TokenType.ADDOP, Character.toString(currentChar)));
+                            position++;
+                        }
+                        break;
                     case '*':
                     case '/':
-                        tokens.add(new Token(TokenType.ADDOP_MULOP, Character.toString(currentChar)));
-                        position++;
+                    case '&':
+                        if (currentChar == '&' && position + 1 < input.length() && input.charAt(position + 1) == '&') {
+                            tokens.add(new Token(TokenType.MULOP, "&&"));
+                            position += 2;
+                        } else {
+                            tokens.add(new Token(TokenType.MULOP, Character.toString(currentChar)));
+                            position++;
+                        }
                         break;
                     case '(':
                     case ')':
@@ -79,7 +105,7 @@ public class LexicalAnalyzer {
                         tokens.add(new Token(TokenType.SYMBOL, Character.toString(currentChar)));
                         position++;
                         break;
-		    case '{':
+                    case '{':
                         // Start of literal
                         StringBuilder literal = new StringBuilder();
                         literal.append(currentChar);
@@ -87,11 +113,16 @@ public class LexicalAnalyzer {
                         // Continue adding characters until the end of the literal is found
                         while (position < input.length() && input.charAt(position) != '}') {
                             char nextChar = input.charAt(position);
+				   if (nextChar == '\n') {
+                                System.err.println("Newline in literal at line " + lineNumber);
+                                return tokens;
+				   }
                             if (nextChar >= 0 && nextChar <= 255) { // ASCII printable characters
                                 literal.append(nextChar);
                                 position++;
                             } else {
-                                throw new IllegalArgumentException("Non-ASCII character in literal.");
+                                System.err.println("Non-ASCII character in literal at line " + lineNumber);
+                                return tokens;
                             }
                         }
                         if (position < input.length() && input.charAt(position) == '}') {
@@ -99,11 +130,13 @@ public class LexicalAnalyzer {
                             tokens.add(new Token(TokenType.LITERAL, literal.toString()));
                             position++;
                         } else {
-                            throw new IllegalArgumentException("Unclosed literal.");
+                            System.err.println("Unclosed literal at line " + lineNumber);
+                            return tokens;
                         }
                         break;
                     default:
-                        throw new IllegalArgumentException("Invalid character: " + currentChar);
+                        System.err.println("Invalid character: " + currentChar + " at line " + lineNumber);
+                        return tokens;
                 }
             }
         }
@@ -111,26 +144,41 @@ public class LexicalAnalyzer {
         return tokens;
     }
     
+    private void prepopulateSymbolTable() {
+        // Keywords
+        symbolTable.put("app", TokenType.KEYWORD);
+        symbolTable.put("var", TokenType.KEYWORD);
+        symbolTable.put("integer", TokenType.KEYWORD);
+        symbolTable.put("real", TokenType.KEYWORD);
+        symbolTable.put("init", TokenType.KEYWORD);
+        symbolTable.put("return", TokenType.KEYWORD);
+        symbolTable.put("if", TokenType.KEYWORD);
+        symbolTable.put("then", TokenType.KEYWORD);
+        symbolTable.put("else", TokenType.KEYWORD);
+        symbolTable.put("end", TokenType.KEYWORD);
+        symbolTable.put("repeat", TokenType.KEYWORD);
+        symbolTable.put("until", TokenType.KEYWORD);
+        symbolTable.put("read", TokenType.KEYWORD);
+        symbolTable.put("write", TokenType.KEYWORD);
+        // Relational operators
+        symbolTable.put("=", TokenType.RELOP);
+        symbolTable.put(">", TokenType.RELOP);
+        symbolTable.put(">=", TokenType.RELOP);
+        symbolTable.put("<", TokenType.RELOP);
+        symbolTable.put("<=", TokenType.RELOP);
+        symbolTable.put("!=", TokenType.RELOP);
+        // Additive operators
+        symbolTable.put("+", TokenType.ADDOP);
+        symbolTable.put("-", TokenType.ADDOP);
+        symbolTable.put("||", TokenType.ADDOP);
+        // Multiplicative operators
+        symbolTable.put("*", TokenType.MULOP);
+        symbolTable.put("/", TokenType.MULOP);
+        symbolTable.put("&&", TokenType.MULOP);
+    }
+    
     private boolean isKeyword(String tokenValue) {
-        switch (tokenValue) {
-            case "app":
-            case "var":
-            case "integer":
-            case "real":
-            case "init":
-            case "return":
-            case "if":
-            case "then":
-            case "else":
-            case "end":
-            case "repeat":
-            case "until":
-            case "read":
-            case "write":
-                return true;
-            default:
-                return false;
-        }
+        return symbolTable.containsKey(tokenValue) && symbolTable.get(tokenValue) == TokenType.KEYWORD;
     }
 }
 
@@ -159,7 +207,8 @@ enum TokenType {
     SYMBOL,
     OPERATOR,
     RELOP,
-    ADDOP_MULOP,
+    ADDOP,
+    MULOP,
     ASSIGN_OP,
     LITERAL
 }
